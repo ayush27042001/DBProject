7517eac281b5ce4427b6a76a225be1b379b7280e
Ayush Awasthi <ayush.awasthi@idemia.com>
2023-10-16 00:13:24 +0530

    my first commit

---
+++ Solution1/Database1/dbo/Tables/tbl_product.sql
@@ -0,0 +1,6 @@
+﻿CREATE TABLE [dbo].[tbl_product] (
+    [id]   INT           IDENTITY (1, 1) NOT NULL,
+    [name] VARCHAR (100) NULL,
+    CONSTRAINT [PK_dbo.tbl_product] PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
